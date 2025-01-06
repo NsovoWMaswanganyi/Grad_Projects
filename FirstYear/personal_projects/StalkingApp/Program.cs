6f427d32e6266d9553b91475375f48de405613e1
Nsovo W. Maswanganyi <148325838+NsovoWMaswanganyi@users.noreply.github.com>
2025-01-06 14:41:12 +0200

    Update Program.cs

--- FirstYear/personal_projects/StalkingApp/Program.cs
+++ FirstYear/personal_projects/StalkingApp/Program.cs
@@ -1,4 +1,33 @@
-﻿using System;
+/*
+Nsovo W. Maswanganyi
+Exit 0
+
+This program, as unethical as it may sound, was written so i could check who in my first-year programming class had passed or not( I was bored what do you expect from a 2 month summer break).
+A lot is going on here, so i don't expect anyone to read and understand this code.
+I don't think I'll be able to understand it myself after a few months(I whipped it in less than 3 hours).
+
+The varibles and the names of other properties are supposed to be changed(I hope I do this before you can see the mess)
+because there was a lot of trial and error and the original idea changed while i was coding. So yeah, some of the names you see here
+will need you to have some superpowers to understand what they are doing
+
+
+With that said, if you can read it and understand it, be sure to comment on where I could've done better
+I only wrote this code for a single purpose - for it TO RUN!
+
+But if you don't care too much to understand it, but just want to use it to stalk your fellow classmates,
+here are a few things you must do:
+
+- Provide the program with your list of current and previous period students(I've used text files for this - look for ...txt in my code)
+- Make sure the location of the txt files are located in the correct directory; i.e inside "bin/Debug/here.txt"
+- RUN IT!
+
+Future Plans for the Stalking App
+Well, so far this app is just a console app, so I'm thinking of writing another program, but in Django this time,
+so i can see the data on the web.
+
+So yeah, Good Luck!!
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
